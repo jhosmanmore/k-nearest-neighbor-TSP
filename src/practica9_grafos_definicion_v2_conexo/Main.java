8d19316186c5f5e87cd9cc3f76dc792ea94009d5
Jhosman Moreno <jhosmanmoreno@gmail.com>
2024-05-12 22:28:18 -0500

    v1

---
+++ src/practica9_grafos_definicion_v2_conexo/Main.java
@@ -0,0 +1,13 @@
+/* UNIVERSIDAD DE PAMPLONA
+    Ingeniería de Sistemas - Matemáticas discretas: Grafos
+    M.Sc. Luis Armando Portilla Granados
+    Abril 24 de 2018 (Actualizado octubre de 2022: ArrayList)
+*/
+
+package practica9_grafos_definicion_v2_conexo;
+public class Main {
+    public static void main(String[] args) {
+        Ventana_v2_conexo objeto= new Ventana_v2_conexo();
+        objeto.setVisible(true);
+    }
+}
